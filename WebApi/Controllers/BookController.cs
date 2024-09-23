c10019bfe28015b465b4776d8daa5b320e3d14bf
Berk Akın <berkakin99@hotmail.com>
2024-09-23 18:29:16 +0300

    UPDATE: book addition and getting books is now on logical layer and this file calls them

--- WebApi/Controllers/BookController.cs
+++ WebApi/Controllers/BookController.cs
@@ -2,6 +2,9 @@ using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using WebApi.BookOperations.CreateBook;
+using WebApi.BookOperations.GetBooks;
+using WebApi.DBOperations;
 
 namespace WebApi.Controllers
 {
@@ -9,78 +12,53 @@ namespace WebApi.Controllers
     [Route("[controller]s")]
     public class BookController : ControllerBase
     {
-        private static List<Book> BookList = new List<Book>(){
-            new Book{
-                Id=1,
-                Title="Lean Startup",
-                GenreId=1,
-                PageCount=200, //Personal Growth
-                PublishDate = new DateTime(2001,09,02),
-                Author="Berk Akın"
-            },
-            new Book{
-                Id=2,
-                Title="Chess",
-                GenreId=3, //Drama
-                PageCount=102,
-                PublishDate = new DateTime(1980,06,12),
-                Author="Stefan Zweig"
-            },
-            new Book{
-                Id=3,
-                Title="Dune",
-                GenreId=3, //Sci-fi
-                PageCount=540,
-                PublishDate = new DateTime(2045,02,22),
-                Author="Stefan Zweig"
-            },
-        };
+        private readonly BookStoreDbContext _context;
+
+        public BookController(BookStoreDbContext context)
+        {
+            _context = context;
+        }
 
         [HttpGet]
-        public List<Book> GetBooks()
+        public IActionResult GetBooks()
         {
-            var bookList = BookList.OrderBy(x => x.Id).ToList<Book>();
-            return bookList;
+            GetBooksQuery query = new GetBooksQuery(_context);
+            var result = query.Handle();
+            return Ok(result);
         }
 
         [HttpGet("{Id}")]
         public Book GetBooksById(int Id)
         {
-            var book = BookList.Where(book => book.Id == Id).SingleOrDefault();
+            var book = _context.Books.Where(book => book.Id == Id).SingleOrDefault();
             return book;
         }
 
-        /*    ROUTE'TAN ALIR ID BİLGİSİNİ
-                [HttpGet]
-                public Book Get([FromQuery] string Id)
-                {
-                    var book = BookList.Where(book => book.Id == Convert.ToInt32(Id)).SingleOrDefault();
-                    return book;
-                } 
-        */
 
         //Post ve Put Örnekleri
 
         //Book'un kalıbına uygun bir json gelmeli front-end tarafından body bilgisi olarak gelecek bu sayede otomatik olarak requesti alacak ve işlemleri yapacak
         [HttpPost]
-        public IActionResult AddBook([FromBody] Book newBook)
+        public IActionResult AddBook([FromBody] CreateBookModel newBook)
         {
-            var book = BookList.SingleOrDefault(x => x.Title == newBook.Title);
-            if (book is not null)
+            try
             {
-                return BadRequest();
+                CreateBookCommand command = new CreateBookCommand(_context);
+                command.Model = newBook;
+                command.Handle();
+                return Ok("Kitap Eklendi");
             }
-            else
+            catch (Exception ex)
             {
-                BookList.Add(newBook);
-                return Ok();
+                return BadRequest(ex.Message);
             }
+
         }
 
         [HttpPut("{id}")]
         public IActionResult UpdateBook(int id, [FromBody] Book updatedBook)
         {
-            var book = BookList.SingleOrDefault(x => x.Id == id);
+            var book = _context.Books.SingleOrDefault(x => x.Id == id);
             if (book is not null)
             {
                 book.Title = updatedBook.Title != default ? updatedBook.Title : book.Title;
@@ -99,10 +77,10 @@ namespace WebApi.Controllers
         [HttpDelete("{id}")]
         public IActionResult DeleteBook(int id)
         {
-            var book = BookList.SingleOrDefault(x => x.Id == id);
+            var book = _context.Books.SingleOrDefault(x => x.Id == id);
             if (book is not null)
             {
-                BookList.Remove(book);
+                _context.Books.Remove(book);
                 return Ok();
             }
             else
