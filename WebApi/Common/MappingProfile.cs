136a448c52137b8ac53345a49803d8b08f350e1c
Berk Akın <berkakin99@hotmail.com>
2024-09-26 01:10:54 +0300

    FEAT: Added mapping config for getting all books detail.

--- WebApi/Common/MappingProfile.cs
+++ WebApi/Common/MappingProfile.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using AutoMapper;
 using WebApi.BookOperations.CreateBook;
 using WebApi.BookOperations.GetBooks;
@@ -10,6 +11,7 @@ namespace WebApi.Common
         {
             CreateMap<CreateBookModel, Book>();
             CreateMap<Book, BookDetailViewModel>().ForMember(dest => dest.Genre, opt => opt.MapFrom(src => ((GenreEnum)src.GenreId).ToString()));
+            CreateMap<Book, BooksViewModel>().ForMember(dest => dest.Genre, opt => opt.MapFrom(src => ((GenreEnum)src.GenreId).ToString()));
         }
     }
 }
