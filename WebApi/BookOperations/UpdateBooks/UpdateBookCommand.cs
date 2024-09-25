08f221ea3370db0bc5a99d4e446b4047ab168efe
Berk Akın <berkakin99@hotmail.com>
2024-09-25 20:05:00 +0300

    feat: updating book command implemented. It's now using a model when returning data to prevent returning all entity model

---
+++ WebApi/BookOperations/UpdateBooks/UpdateBookCommand.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Linq;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using WebApi.BookOperations.GetBooks;
+using WebApi.DBOperations;
+
+namespace WebApi.BookOperations.UpdateBook
+{
+    class UpdateBookCommand
+    {
+        private readonly BookStoreDbContext _dbContext;
+        public int BookId;
+        public UpdateBookModel Model { get; set; }
+        public UpdateBookCommand(BookStoreDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public void Handle()
+        {
+            var book = _dbContext.Books.SingleOrDefault(x => x.Id == BookId);
+            if (book is not null)
+            {
+                book.Title = Model.Title != default ? Model.Title : book.Title;
+                book.GenreId = Model.GenreId != default ? Model.GenreId : book.GenreId;
+                _dbContext.SaveChanges();
+            }
+            else
+            {
+                throw new InvalidOperationException("Kitap bulunamadı. Güncelleme İşleminde Hata Oldu");
+            }
+        }
+
+
+    }
+
+    public class UpdateBookModel
+    {
+        public string Title { get; set; }
+        public int GenreId { get; set; }
+    }
+}
