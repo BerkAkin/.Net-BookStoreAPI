5e5bf54b32357f4af8346f713055af68a2a120dc
Berk Akın <berkakin99@hotmail.com>
2024-09-26 05:42:00 +0300

    FEAT: Validation rules for posting book route defined

---
+++ WebApi/BookOperations/CreateBook/CreateBookCommandValidator.cs
@@ -0,0 +1,16 @@
+using System;
+using FluentValidation;
+
+namespace WebApi.BookOperations.CreateBook
+{
+    public class CreateBookCommandValidator : AbstractValidator<CreateBookCommand>
+    {
+        public CreateBookCommandValidator()
+        {
+            RuleFor(command => command.Model.GenreId).GreaterThan(0).NotEmpty().NotNull();
+            RuleFor(command => command.Model.PageCount).GreaterThan(0).NotEmpty().NotNull();
+            RuleFor(command => command.Model.PublishDate.Date).NotEmpty().NotNull().LessThan(DateTime.Now.Date);
+            RuleFor(command => command.Model.Title).NotEmpty().NotNull().MinimumLength(4);
+        }
+    }
+}
