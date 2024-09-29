8f5b654e4ee1c52853ab80bf6b8f7f013aeea176
Berk Akın <berkakin99@hotmail.com>
2024-09-29 20:40:30 +0300

    cache update after updating endpoint validation rules

--- WebApi/obj/Debug/net5.0/WebApi.AssemblyInfo.cs
+++ WebApi/obj/Debug/net5.0/WebApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+230b6ac42fabcadccd2b087008831345bbb17b73")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e0a6336d0dbff7cf990888b3007c9532b7df5ba5")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
