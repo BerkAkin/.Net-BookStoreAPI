38f23bf7dbb3935c381fef971a720475ed70164e
Berk Akın <berkakin99@hotmail.com>
2024-09-30 04:02:57 +0300

    FEAT: added response message log and elapsed time formatted on miliseconds

--- WebApi/Middlewares/CustomExceptionMiddleware.cs
+++ WebApi/Middlewares/CustomExceptionMiddleware.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Diagnostics;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 
@@ -13,9 +14,13 @@ namespace WebApi.Middlewares
         }
         public async Task Invoke(HttpContext context)
         {
+            var watch = Stopwatch.StartNew();
             string message = "[Request] HTTP " + context.Request.Method + " - " + context.Request.Path;
             Console.WriteLine(message);
             await _next(context);
+            watch.Stop();
+            message = "[Response] HTTP " + context.Request.Method + " - " + context.Request.Path + " responded " + context.Response.StatusCode + " in " + watch.Elapsed.TotalMilliseconds + " ms";
+            Console.WriteLine(message);
 
         }
     }
