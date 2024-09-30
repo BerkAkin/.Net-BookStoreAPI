9f255a06a22f7350d33cba075cc2fc588950cab1
Berk Akın <berkakin99@hotmail.com>
2024-09-30 07:24:49 +0300

    REFACTOR: Errors are now handling from CustomExceptionMiddleware

--- WebApi/Middlewares/CustomExceptionMiddleware.cs
+++ WebApi/Middlewares/CustomExceptionMiddleware.cs
@@ -1,7 +1,10 @@
 using System;
 using System.Diagnostics;
+using System.IO;
+using System.Net;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
 
 namespace WebApi.Middlewares
 {
@@ -15,12 +18,33 @@ namespace WebApi.Middlewares
         public async Task Invoke(HttpContext context)
         {
             var watch = Stopwatch.StartNew();
-            string message = "[Request]  HTTP " + context.Request.Method + " - " + context.Request.Path;
+            try
+            {
+                string message = "[Request]  HTTP " + context.Request.Method + " - " + context.Request.Path;
+                Console.WriteLine(message);
+                await _next(context);
+                watch.Stop();
+                message = "[Response] HTTP " + context.Request.Method + " - " + context.Request.Path + " responded " + context.Response.StatusCode + " in " + watch.Elapsed.TotalMilliseconds + " ms";
+                Console.WriteLine(message);
+            }
+            catch (Exception ex)
+            {
+                watch.Stop();
+                await HandleException(context, ex, watch);
+            }
+
+
+        }
+        private Task HandleException(HttpContext context, Exception ex, Stopwatch watch)
+        {
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+
+            string message = "[Error]   HTTP " + context.Request.Method + " - " + context.Response.StatusCode + " Error Message " + ex.Message + " in " + watch.Elapsed.TotalMilliseconds + " ms";
             Console.WriteLine(message);
-            await _next(context);
-            watch.Stop();
-            message = "[Response] HTTP " + context.Request.Method + " - " + context.Request.Path + " responded " + context.Response.StatusCode + " in " + watch.Elapsed.TotalMilliseconds + " ms";
-            Console.WriteLine(message);
+
+            var result = JsonConvert.SerializeObject(new { error = ex.Message, }, Formatting.None);
+            return context.Response.WriteAsync(result);
 
         }
     }
