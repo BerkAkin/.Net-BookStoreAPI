8eefde8b8d3513829266680bd1220860417e1c2d
Berk Akın <berkakin99@hotmail.com>
2024-10-03 14:47:50 +0300

    FEAT: created update genre command validator

---
+++ WebApi/Application/GenreOperations/Commands/UpdateGenre/UpdateGenreCommandValidator.cs
@@ -0,0 +1,13 @@
+
+using FluentValidation;
+
+namespace WebApi.Application.GenreOperations.Commands.UpdateGenre
+{
+    public class UpdateGenreCommandValidator : AbstractValidator<UpdateGenreCommand>
+    {
+        public UpdateGenreCommandValidator()
+        {
+            RuleFor(command => command.Model.Name).NotEmpty().MinimumLength(4);
+        }
+    }
+}
