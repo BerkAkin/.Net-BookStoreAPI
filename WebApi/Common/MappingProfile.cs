c2716602dc5739758119a16e130d5e8f97ab1c3c
Berk Akın <berkakin99@hotmail.com>
2024-10-03 15:46:20 +0300

    REFACTOR: Genre is now inner joined as table to Book Table and removed static Enum Data

--- WebApi/Common/MappingProfile.cs
+++ WebApi/Common/MappingProfile.cs
@@ -5,15 +5,14 @@ using WebApi.Application.BookOperations.Queries.GetBooks;
 using WebApi.Application.GenreOperations.Queries.GetGenres;
 using WebApi.Application.GenreOperations.Queries.GetGenreDetail;
 using WebApi.Entities;
-using WebApi.Application.GenreOperations.Commands.CreateGenre;
 namespace WebApi.Common
 {
     public class MappingProfile : Profile
     {
         public MappingProfile()
         {
-            CreateMap<Book, BookDetailViewModel>().ForMember(dest => dest.Genre, opt => opt.MapFrom(src => ((GenreEnum)src.GenreId).ToString()));
-            CreateMap<Book, BooksViewModel>().ForMember(dest => dest.Genre, opt => opt.MapFrom(src => ((GenreEnum)src.GenreId).ToString()));
+            CreateMap<Book, BookDetailViewModel>().ForMember(dest => dest.Genre, opt => opt.MapFrom(src => src.Genre.Name));
+            CreateMap<Book, BooksViewModel>().ForMember(dest => dest.Genre, opt => opt.MapFrom(src => src.Genre.Name));
             CreateMap<CreateBookModel, Book>();
 
             CreateMap<Genre, GenresViewModel>();
