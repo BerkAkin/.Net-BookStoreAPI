803387ae9b39f52e8d948317bafad653ae9f6cf1
Berk Akın <berkakin99@hotmail.com>
2024-10-03 13:54:02 +0300

    FIX: Typo on class namespace

--- WebApi/Common/MappingProfile.cs
+++ WebApi/Common/MappingProfile.cs
@@ -5,15 +5,17 @@ using WebApi.Application.BookOperations.Queries.GetBooks;
 using WebApi.Application.GenreOperations.Queries.GetGenres;
 using WebApi.Application.GenreOperations.Queries.GetGenreDetail;
 using WebApi.Entities;
+using WebApi.Application.GenreOperations.Commands.CreateGenre;
 namespace WebApi.Common
 {
     public class MappingProfile : Profile
     {
         public MappingProfile()
         {
-            CreateMap<CreateBookModel, Book>();
             CreateMap<Book, BookDetailViewModel>().ForMember(dest => dest.Genre, opt => opt.MapFrom(src => ((GenreEnum)src.GenreId).ToString()));
             CreateMap<Book, BooksViewModel>().ForMember(dest => dest.Genre, opt => opt.MapFrom(src => ((GenreEnum)src.GenreId).ToString()));
+            CreateMap<CreateBookModel, Book>();
+
             CreateMap<Genre, GenresViewModel>();
             CreateMap<Genre, GenreDetailViewModel>();
         }
