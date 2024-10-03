66dc7d7953f74bb0005b472250dfe1e6133d6890
Berk Akın <berkakin99@hotmail.com>
2024-10-03 14:49:26 +0300

    REFACTOR: where clause on genre select command changed to singleordefault

--- WebApi/Application/GenreOperations/Commands/DeleteGenre/DeleteGenreCommand.cs
+++ WebApi/Application/GenreOperations/Commands/DeleteGenre/DeleteGenreCommand.cs
@@ -16,7 +16,7 @@ namespace WebApi.Application.GenreOperations.Commands.DeleteGenre
 
         public void Handle()
         {
-            var genre = _context.Genres.Where(x => x.Id == GenreId).FirstOrDefault();
+            var genre = _context.Genres.SingleOrDefault(x => x.Id == GenreId);
             if (genre == null)
             {
                 throw new InvalidOperationException("Tür Bulunamadı, silme başarısız");
