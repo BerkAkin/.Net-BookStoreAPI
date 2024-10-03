b724f297a12ebd5777c76005760c38462e203a11
Berk Akın <berkakin99@hotmail.com>
2024-10-03 15:46:02 +0300

    REFACTOR: Genre is now inner joined as table to Book Table

--- WebApi/Application/BookOperations/Queries/GetBookDetail/GetBookDetailQuery.cs
+++ WebApi/Application/BookOperations/Queries/GetBookDetail/GetBookDetailQuery.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using AutoMapper;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using WebApi.Common;
 using WebApi.DBOperations;
@@ -22,7 +23,7 @@ namespace WebApi.Application.BookOperations.Queries.GetBooks
 
         public BookDetailViewModel Handle()
         {
-            var book = _dbContext.Books.Where(book => book.Id == BookId).SingleOrDefault();
+            var book = _dbContext.Books.Include(x => x.Genre).Where(book => book.Id == BookId).SingleOrDefault();
             if (book is not null)
             {
                 BookDetailViewModel vm = _mapper.Map<BookDetailViewModel>(book);
