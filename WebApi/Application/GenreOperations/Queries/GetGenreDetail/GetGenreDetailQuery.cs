1eed3278a40cc6b0b50f6ee3b81f70f5bf6f629b
Berk Akın <berkakin99@hotmail.com>
2024-10-03 13:20:43 +0300

    FEAT: Created Genre Detail Query Operation and Genre Detail View Model Class for automapper

---
+++ WebApi/Application/GenreOperations/Queries/GetGenreDetail/GetGenreDetailQuery.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Linq;
+using AutoMapper;
+using WebApi.DBOperations;
+
+namespace WebApi.Application.GenreOperations.Queries.GetGenreDetail
+{
+    public class GetGenresDetailQuery
+    {
+
+        public GenreDetailViewModel Model { get; set; }
+        private readonly BookStoreDbContext _context;
+        private readonly IMapper _mapper;
+        public int GenreId { get; set; }
+
+        public GetGenresDetailQuery(BookStoreDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        public GenreDetailViewModel Handle()
+        {
+            var genre = _context.Genres.SingleOrDefault(x => x.Id == GenreId && x.IsActive);
+            if (genre == null)
+            {
+                throw new InvalidOperationException("Tür Bulunamadı");
+            }
+            else
+            {
+                return _mapper.Map<GenreDetailViewModel>(genre);
+            }
+        }
+    }
+    public class GenreDetailViewModel
+    {
+        public int Id { get; set; }
+        public int Name { get; set; }
+    }
+
+
+}
