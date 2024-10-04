016212b685f76ed71390c033deb4a0820006cd96
Berk Akın <berkakin99@hotmail.com>
2024-10-04 13:08:54 +0300

    FEAT: Author Creator Command implemented

---
+++ WebApi/Application/AuthorOperations/Commands/CreateAuthor/CreateAuthorCommand.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Linq;
+using AutoMapper;
+using WebApi.DBOperations;
+using WebApi.Entities;
+
+namespace WebApi.Application.AuthorOperations.Commands.CreateAuthor
+{
+    public class CreateAuthorCommand
+    {
+        private readonly IMapper _mapper;
+        private readonly BookStoreDbContext _context;
+        public CreateAuthorViewModel Model { get; set; }
+
+        public CreateAuthorCommand(IMapper mapper, BookStoreDbContext context)
+        {
+            _mapper = mapper;
+            _context = context;
+        }
+        public void Handle()
+        {
+            var author = _context.Authors.SingleOrDefault(x => x.Name == Model.Name && x.Surname == Model.Surname);
+            if (author != null)
+            {
+                throw new InvalidOperationException("Yazar Zaten Mevcut");
+            }
+            else
+            {
+                author = _mapper.Map<Author>(Model);
+                _context.Authors.Add(author);
+                _context.SaveChanges();
+            }
+        }
+    }
+
+    public class CreateAuthorViewModel
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public DateTime BirthDate { get; set; }
+    }
+}
