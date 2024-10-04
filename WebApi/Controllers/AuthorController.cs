a540e1859a40bd5ceb4a77fa3032320d6fd7f715
Berk Akın <berkakin99@hotmail.com>
2024-10-04 13:10:52 +0300

    FEAT: Author Endpoints defined

---
+++ WebApi/Controllers/AuthorController.cs
@@ -0,0 +1,101 @@
+using AutoMapper;
+using FluentValidation;
+using FluentValidation.Results;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using WebApi.Application.AuthorOperations.Commands.CreateAuthor;
+using WebApi.Application.AuthorOperations.Commands.DeleteAuthor;
+using WebApi.Application.AuthorOperations.Commands.UpdateAuthor;
+using WebApi.Application.AuthorOperations.Queries.GetAuthorDetail;
+using WebApi.Application.AuthorOperations.Queries.GetAuthors;
+using WebApi.Application.GenreOperations.Commands.CreateGenre;
+using WebApi.Application.GenreOperations.Commands.DeleteGenre;
+using WebApi.Application.GenreOperations.Commands.UpdateGenre;
+using WebApi.Application.GenreOperations.Queries.GetGenreDetail;
+using WebApi.Application.GenreOperations.Queries.GetGenres;
+using WebApi.DBOperations;
+
+namespace WebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]s")]
+    public class AuthorController : ControllerBase
+    {
+        private readonly BookStoreDbContext _context;
+        private readonly IMapper _mapper;
+
+        public AuthorController(BookStoreDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+
+
+
+        [HttpGet]
+        public IActionResult GetAuthor()
+        {
+            GetAuthorsQuery query = new GetAuthorsQuery(_mapper, _context);
+            var result = query.Handle();
+            return Ok(result);
+        }
+
+
+
+
+
+        [HttpGet("{id}")]
+        public IActionResult GetAuthorById(int id)
+        {
+            AuthorDetailViewModel result;
+            GetAuthorDetailQuery query = new GetAuthorDetailQuery(_mapper, _context);
+            query.Id = id;
+            result = query.Handle();
+            return Ok(result);
+        }
+
+
+
+
+
+        [HttpPost]
+        public IActionResult AddAuthor([FromBody] CreateAuthorViewModel author)
+        {
+            CreateAuthorCommand command = new CreateAuthorCommand(_mapper, _context);
+            CreateAuthorCommandValidator validator = new CreateAuthorCommandValidator();
+            command.Model = author;
+            validator.ValidateAndThrow(command);
+            command.Handle();
+            return Ok("Ekleme Başarılı");
+        }
+
+
+
+
+        [HttpPut("{id}")]
+        public IActionResult UpdateAuthor([FromBody] UpdateAuthorViewModel updatedAuthor, int id)
+        {
+            UpdateAuthorCommand command = new UpdateAuthorCommand(_context, _mapper);
+            command.Id = id;
+            command.Model = updatedAuthor;
+            UpdateAuthorCommandValidator validator = new UpdateAuthorCommandValidator();
+            validator.ValidateAndThrow(command);
+            command.Handle();
+            return Ok("Güncelleme Başarılı");
+        }
+
+
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteAuthor(int id)
+        {
+            DeleteAuthorCommand command = new DeleteAuthorCommand(_context);
+            command.Id = id;
+            command.Handle();
+            return Ok("Silme Başarılı");
+        }
+    }
+}
