586e7e52cbc94f9c018e980ae3dd96d334828921
Berk Akın <berkakin99@hotmail.com>
2024-10-04 13:09:19 +0300

    FEAT: Author Updated Command implemented

---
+++ WebApi/Application/AuthorOperations/Commands/UpdateAuthor/UpdateAuthorCommand.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Linq;
+using AutoMapper;
+using WebApi.DBOperations;
+using WebApi.Entities;
+
+
+namespace WebApi.Application.AuthorOperations.Commands.UpdateAuthor
+{
+    public class UpdateAuthorCommand
+    {
+
+        private readonly BookStoreDbContext _context;
+        private readonly IMapper _mapper;
+        public int Id { get; set; }
+        public UpdateAuthorViewModel Model { get; set; }
+
+        public UpdateAuthorCommand(BookStoreDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        public void Handle()
+        {
+            var author = _context.Authors.SingleOrDefault(x => x.Id == Id);
+            if (author == null)
+            {
+                throw new InvalidOperationException("Güncellenecek yazar bulunamadı");
+            }
+            else
+            {
+                author.Name = Model.Name != default ? Model.Name : author.Name;
+                author.Surname = Model.Surname != default ? Model.Surname : author.Surname;
+                author.BirthDate = Model.BirthDate != default ? Model.BirthDate : author.BirthDate;
+                _context.SaveChanges();
+            }
+        }
+    }
+
+    public class UpdateAuthorViewModel
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public DateTime BirthDate { get; set; }
+    }
+}
