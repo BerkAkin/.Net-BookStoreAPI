4a4f00793c5d9ca0dee63fa4e75d95a08d0526e9
Berk Akın <berkakin99@hotmail.com>
2024-10-04 13:10:36 +0300

    FEAT: added Author Mapping configs

--- WebApi/Common/MappingProfile.cs
+++ WebApi/Common/MappingProfile.cs
@@ -1,10 +1,12 @@
-using System.Collections.Generic;
 using AutoMapper;
 using WebApi.Application.BookOperations.Commands.CreateBook;
 using WebApi.Application.BookOperations.Queries.GetBooks;
 using WebApi.Application.GenreOperations.Queries.GetGenres;
 using WebApi.Application.GenreOperations.Queries.GetGenreDetail;
 using WebApi.Entities;
+using WebApi.Application.AuthorOperations.Queries.GetAuthors;
+using WebApi.Application.AuthorOperations.Queries.GetAuthorDetail;
+using WebApi.Application.AuthorOperations.Commands.CreateAuthor;
 namespace WebApi.Common
 {
     public class MappingProfile : Profile
@@ -17,6 +19,10 @@ namespace WebApi.Common
 
             CreateMap<Genre, GenresViewModel>();
             CreateMap<Genre, GenreDetailViewModel>();
+
+            CreateMap<Author, AuthorsViewModel>();
+            CreateMap<Author, AuthorDetailViewModel>();
+            CreateMap<CreateAuthorViewModel, Author>();
         }
     }
 }
