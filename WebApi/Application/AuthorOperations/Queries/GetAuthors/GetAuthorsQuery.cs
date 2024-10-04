e4698238c3e70837abc019f92735e5affc4901d7
Berk Akın <berkakin99@hotmail.com>
2024-10-04 13:09:45 +0300

    FEAT: Authors Viewer Command implemented

---
+++ WebApi/Application/AuthorOperations/Queries/GetAuthors/GetAuthorsQuery.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using AutoMapper;
+using WebApi.DBOperations;
+using WebApi.Entities;
+
+namespace WebApi.Application.AuthorOperations.Queries.GetAuthors
+{
+    public class GetAuthorsQuery
+    {
+        private readonly IMapper _mapper;
+        private readonly BookStoreDbContext _context;
+        public GetAuthorsQuery(IMapper mapper, BookStoreDbContext context)
+        {
+            _mapper = mapper;
+            _context = context;
+        }
+
+        public List<AuthorsViewModel> Handle()
+        {
+            List<Author> authors = _context.Authors.ToList<Author>();
+            List<AuthorsViewModel> vm = _mapper.Map<List<AuthorsViewModel>>(authors);
+            return vm;
+        }
+    }
+
+    public class AuthorsViewModel
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public DateTime BirthDate { get; set; }
+    }
+}
