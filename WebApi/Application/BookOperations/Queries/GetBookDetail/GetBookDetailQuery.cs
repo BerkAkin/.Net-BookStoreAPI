8b2b37131e765c9cca0f2fc53ddbdcf7a78e479c
Berk Akın <berkakin99@hotmail.com>
2024-10-04 13:10:24 +0300

    Refactor: Removed unused libraries

--- WebApi/Application/BookOperations/Queries/GetBookDetail/GetBookDetailQuery.cs
+++ WebApi/Application/BookOperations/Queries/GetBookDetail/GetBookDetailQuery.cs
@@ -1,10 +1,7 @@
 using System;
-using System.Collections.Generic;
 using System.Linq;
 using AutoMapper;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Metadata.Internal;
-using WebApi.Common;
 using WebApi.DBOperations;
 
 namespace WebApi.Application.BookOperations.Queries.GetBooks
