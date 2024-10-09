0c978e470e3da74bb8b8f3c1fd259f701124c797
Berk Akın <berkakin99@hotmail.com>
2024-10-09 14:25:41 +0300

    FEAT: created user and token endpoints

---
+++ WebApi/Controllers/UserController.cs
@@ -0,0 +1,45 @@
+using AutoMapper;
+using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
+using WebApi.Application.UserOperations.Commands.CreateUser;
+using WebApi.DBOperations;
+using WebApi.TokenOperations.Models;
+using WebApi.Application.UserOperations.Commands;
+
+namespace WebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]s")]
+    public class UserController : ControllerBase
+    {
+        private readonly IBookStoreDbContext _context;
+        private readonly IMapper _mapper;
+        private readonly IConfiguration _configuration;
+        public UserController(IBookStoreDbContext context, IMapper mapper, IConfiguration configuration)
+        {
+            _context = context;
+            _mapper = mapper;
+            _configuration = configuration;
+        }
+
+        [HttpPost]
+        public IActionResult Create([FromBody] CreateUserModel newUser)
+        {
+            CreateUserCommand command = new CreateUserCommand(_context, _mapper);
+            command.Model = newUser;
+            command.Handle();
+
+            return Ok();
+        }
+
+        [HttpPost("connect/token")]
+        public ActionResult<Token> CreateToken([FromBody] CreateTokenModel login)
+        {
+            CreateTokenCommand command = new CreateTokenCommand(_context, _mapper, _configuration);
+            command.Model = login;
+            var token = command.Handle();
+            return token;
+        }
+    }
+}
